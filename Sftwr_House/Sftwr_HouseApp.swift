6274eaf58527ed961d436091f9847691defd265b
fats4 <muhammadfatu.2@gmail.com>
2024-06-30 18:43:13 +0700

    Initial Commit

---
+++ Sftwr_House/Sftwr_HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sftwr_HouseApp.swift
+//  Sftwr_House
+//
+//  Created by FaTs on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Sftwr_HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
